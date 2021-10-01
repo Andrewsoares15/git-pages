5e928e88ef82c9dbe78d034ddb12fc7ae58a2783
Andrew soares <andrewsoares347@gmail.com>
2021-10-01 14:42:53 -0300

    versao 1

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Vendas from '../views/Vendas.vue'
+import Sobre from '../views/Sobre.vue'
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/vendas',
+    name: 'Vendas',
+    component: Vendas
+  },
+  {
+    path: '/sobre',
+    name: 'Sobre',
+    component: Sobre
+  },
+
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ src/views/Vendas.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+      <h1>Tela Vendas </h1>
+  </v-container>
+</template>
+
+<script>
+export default {
+    name: 'Vendas'
+}
+</script>
+
+<style>
+
+</style>
